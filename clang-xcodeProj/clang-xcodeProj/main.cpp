6551d24c56d8658167a814b4052b5332a0275197
徐仲平 <zhongping.xu@ushow.media>
2020-09-05 21:46:02 +0800

    xxx

---
+++ clang-xcodeProj/clang-xcodeProj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  clang-xcodeProj
+//
+//  Created by xuzhongping on 2020/9/5.
+//  Copyright © 2020 xuzhongping. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
